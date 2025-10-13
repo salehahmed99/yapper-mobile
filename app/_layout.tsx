bf16177fdc565e555a0dab4a4920c1c9af01efd1
salehahmed99 <saleh.work0004@gmail.com>
2025-10-13 18:48:33 +0300

    feat: add twitter custom fonts and add typography constants

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "PublicSans-ExtraLight": require("../assets/fonts/PublicSans-ExtraLight.ttf"),
+    "PublicSans-Light": require("../assets/fonts/PublicSans-Light.ttf"),
+    "PublicSans-Regular": require("../assets/fonts/PublicSans-Regular.ttf"),
+    "PublicSans-Medium": require("../assets/fonts/PublicSans-Medium.ttf"),
+    "PublicSans-SemiBold": require("../assets/fonts/PublicSans-SemiBold.ttf"),
+    "PublicSans-Bold": require("../assets/fonts/PublicSans-Bold.ttf"),
+    "PublicSans-ExtraBold": require("../assets/fonts/PublicSans-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
---
+++ src/constants/theme.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  typography: {
+    sizes: {
+      tiny: 11,
+      xs: 13,
+      sm: 15,
+      md: 17,
+      lg: 20,
+      xl: 23,
+      xxl: 31,
+    },
+    fonts: {
+      extraLight: "PublicSans-ExtraLight",
+      light: "PublicSans-Light",
+      regular: "PublicSans-Regular",
+      medium: "PublicSans-Medium",
+      semiBold: "PublicSans-SemiBold",
+      bold: "PublicSans-Bold",
+      extraBold: "PublicSans-ExtraBold",
+    },
+  },
+};
