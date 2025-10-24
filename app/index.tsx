b65e7a19aaf91a9f70378e99c46b85ca400b668c
salehahmed99 <saleh.work0004@gmail.com>
2025-10-25 00:34:55 +0300

    bulild: upgrade dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,18 @@
 import { Theme } from '@/src/constants/theme';
 import { useTheme } from '@/src/context/ThemeContext';
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = () => {
   const { theme } = useTheme();
   const styles = useMemo(() => createStyles(theme), [theme]);
+  const { t } = useTranslation();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>HomeScreen</Text>
+
+      <Text style={styles.text}>{t('home.title')}</Text>
     </View>
   );
 };
