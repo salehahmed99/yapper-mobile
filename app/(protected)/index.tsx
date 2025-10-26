ae3237c5d7f27875d97f25a182f091168c35084e
salehahmed99 <saleh.work0004@gmail.com>
2025-10-27 00:58:05 +0300

    refactor: add a placeholder screen for home screen

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Theme } from '@/src/constants/theme';
+import { useTheme } from '@/src/context/ThemeContext';
+import React, { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = () => {
+  const { theme } = useTheme();
+  const { t } = useTranslation();
+  const styles = useMemo(() => createStyles(theme), [theme]);
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{t('home.title')}</Text>
     </View>
   );
 };
 
 export default HomeScreen;
+
+const createStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.colors.background.primary,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      color: theme.colors.text.primary,
+      fontFamily: theme.typography.fonts.bold,
+      fontSize: 18,
+    },
+  });
