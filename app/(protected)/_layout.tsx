3b983556ad56f4209c2a9ee5dd5df73853979262
Karim Yasser <karimmyasserr@gmail.com>
2025-10-29 22:40:26 +0300

    refactor(layout): add authentication checks in ProtectedLayout component

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,14 +1,16 @@
 import AppShell from '@/src/components/shell/AppShell';
+import { useAuthStore } from '@/src/store/useAuthStore';
+import { Redirect } from 'expo-router';
 import React from 'react';
 
 const ProtectedLayout = () => {
-  // const isInitialized = useAuthStore((state) => state.isInitialized);
-  // const isAuthenticated = useAuthStore((state) => state.token !== null);
+  const isInitialized = useAuthStore((state) => state.isInitialized);
+  const isAuthenticated = useAuthStore((state) => state.token !== null);
 
-  // if (!isInitialized) return null;
-  // if (!isAuthenticated) {
-  //   return <Redirect href="/(auth)/login" />;
-  // }
+  if (!isInitialized) return null;
+  if (!isAuthenticated) {
+    return <Redirect href="/(auth)/login" />;
+  }
 
   return <AppShell />;
 };
