fa8451dae3fc44a7c86149ebe62ca281c08024ef
salehahmed99 <saleh.work0004@gmail.com>
2025-11-01 04:39:09 +0200

    fix(tweets): update useTweet hook to handle undefined tweetId

--- src/modules/tweets/hooks/useTweet.ts
+++ src/modules/tweets/hooks/useTweet.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getTweetById } from '../services/tweetService';
 
-export const useTweet = (tweetId: string) => {
+export const useTweet = (tweetId?: string) => {
   return useQuery({
     queryKey: ['tweet', { tweetId }],
-    queryFn: () => getTweetById(tweetId),
+    queryFn: () => getTweetById(tweetId!),
+    enabled: !!tweetId,
   });
 };
