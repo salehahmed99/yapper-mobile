62d2dff3c40a29d199914aaeb7737faa0b56733c
salehahmed99 <saleh.work0004@gmail.com>
2025-11-18 16:36:39 +0200

    refactor: remove unwanted files

--- src/utils/navigation.ts
+++
@@ -1,8 +0,0 @@
-import { Href, Router } from 'expo-router';
-
-export type TweetActivityType = 'likers' | 'retweeters' | 'quotes';
-
-export const navigateToTweetActivity = (router: Router, tweetId: string, type: TweetActivityType) => {
-  const path = `/(protected)/tweets/${tweetId}/${type}` as Href;
-  router.push(path);
-};
