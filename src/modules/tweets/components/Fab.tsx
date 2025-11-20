43d621fdbe439453c1be9c8bee96b56fb16f097c
salehahmed99 <saleh.work0004@gmail.com>
2025-11-20 11:56:35 +0200

    fix: use the new useSpacing hook instead of the useMargins hook

--- src/modules/tweets/components/Fab.tsx
+++ src/modules/tweets/components/Fab.tsx
@@ -1,6 +1,6 @@
 import { Theme } from '@/src/constants/theme';
 import { useTheme } from '@/src/context/ThemeContext';
-import useMargins from '@/src/hooks/useMargins';
+import useSpacing from '@/src/hooks/useSpacing';
 import { Plus } from 'lucide-react-native';
 import React from 'react';
 import { Pressable, StyleSheet } from 'react-native';
@@ -14,7 +14,7 @@ const Fab: React.FC<IFabProps> = ({ onPress }) => {
   const { theme } = useTheme();
   const styles = createStyles(theme);
 
-  const { marginBottom } = useMargins();
+  const { bottom } = useSpacing();
 
   const scale = useSharedValue(1);
 
@@ -33,7 +33,7 @@ const Fab: React.FC<IFabProps> = ({ onPress }) => {
   };
 
   return (
-    <Animated.View style={[styles.fab, { bottom: marginBottom + theme.spacing.sm }, animatedStyle]}>
+    <Animated.View style={[styles.fab, { bottom: bottom + theme.spacing.sm }, animatedStyle]}>
       <Pressable onPress={onPress} onPressIn={handlePressIn} onPressOut={handlePressOut} hitSlop={8}>
         <Plus color={theme.colors.white} />
       </Pressable>
