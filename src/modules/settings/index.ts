6b2115139f788b4abacfe0f2839660004d53a92a
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2025-12-05 13:34:46 +0200

    feat(settings): remove unused SETTINGS_DATA export from settingsConfig

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -2,5 +2,5 @@ export { SettingsTopBar } from './components/SettingsTopBar';
 export { SettingsSearchBar } from './components/SettingsSearchBar';
 export { SettingsSection } from './components/SettingsSection';
 export { SettingsItem } from './components/SettingsItem';
-export { getSettingsData, getYourAccountData, SETTINGS_DATA } from './components/settingsConfig';
+export { getSettingsData, getYourAccountData } from './components/settingsConfig';
 export type { ISettingsItem as SettingsItemType, ISettingsSection as SettingsSectionType } from './types/types';
