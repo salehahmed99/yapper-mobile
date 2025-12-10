578dd0f3633e3bbc0f7c12ca7dcdde074c96ed5b
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2025-12-10 16:42:58 +0200

    fix(theme): update background color to use theme colors in account layout components

--- app/(protected)/(settings)/your-account/account-information/_layout.tsx
+++ app/(protected)/(settings)/your-account/account-information/_layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from 'expo-router';
-
+import { useTheme } from '@/src/context/ThemeContext';
 export default function AccountInformationLayout() {
+  const { theme } = useTheme();
+
   return (
     <Stack
       screenOptions={{
         headerShown: false,
         animation: 'slide_from_right',
-        contentStyle: { backgroundColor: '#000000' },
+        contentStyle: { backgroundColor: theme.colors.background.primary },
       }}
     />
   );
