12a2505ec3bc865d9a6307fd7bfa58e40dc35eec
salehahmed99 <saleh.work0004@gmail.com>
2025-12-11 02:21:35 +0200

    fix: resolve linting issues

--- src/modules/profile/hooks/useFollowersList.ts
+++ src/modules/profile/hooks/useFollowersList.ts
@@ -8,8 +8,10 @@ export const followersKeys = {
   list: (params: IGetFollowersListParams) => [...followersKeys.lists(), params] as const,
 };
 
-interface UseFollowersListOptions
-  extends Omit<UseQueryOptions<IGetFollowersListResponse, Error>, 'queryKey' | 'queryFn'> {
+interface UseFollowersListOptions extends Omit<
+  UseQueryOptions<IGetFollowersListResponse, Error>,
+  'queryKey' | 'queryFn'
+> {
   userId: string;
   cursor?: string;
   limit?: number;
